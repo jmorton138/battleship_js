79d193f827a647915e931035abac46b6898fc924
Jeff Morton <jmorton138@gmail.com>
2022-02-24 16:10:31 +0000

    media queries for responsive layout on mobile

--- src/compBoardView.js
+++ src/compBoardView.js
@@ -1,35 +1,33 @@
-import Game from './Game';
-import displayCellView from './displayCellView';
+import Game from "./Game";
+import displayCellView from "./displayCellView";
 
 const compBoardView = (game) => {
-    const content = document.querySelector("#comp-board");
-    content.innerHTML = "";
-    const compInfo = document.querySelector('.comp-info');
-    compInfo.textContent = `Score: ${game.p2Board.score}`;
-    game.p2Board.grid.forEach((row, x) => {
-        row.forEach((item, y) => {
-            const div = document.createElement('div');
-            const coords = [x, y];
-            div.id = coords;
-            div.className = "cell";
-            
-            div.addEventListener('click', function() {
-                if(game.gameLoop(coords) === "game over") {
-                    compInfo.textContent = `Score: ${game.p2Board.score}`;
-                    return;
-                } else {
-                    compBoardView(game);
-                }
-            });
-            //remove onclick after firing
-            content.appendChild(div);
-            displayCellView(coords, item);
+  const playerBoard = document.querySelector("#player-board");
+  playerBoard.classList.add("pieces-placed");
+  const content = document.querySelector("#comp-board");
+  content.innerHTML = "";
+  const compInfo = document.querySelector(".comp-info");
+  compInfo.textContent = `Score: ${game.p2Board.score}`;
+  game.p2Board.grid.forEach((row, x) => {
+    row.forEach((item, y) => {
+      const div = document.createElement("div");
+      const coords = [x, y];
+      div.id = coords;
+      div.className = "cell";
 
+      div.addEventListener("click", function () {
+        if (game.gameLoop(coords) === "game over") {
+          compInfo.textContent = `Score: ${game.p2Board.score}`;
+          return;
+        } else {
+          compBoardView(game);
         }
-
-        )
-    })
-}
-
+      });
+      //remove onclick after firing
+      content.appendChild(div);
+      displayCellView(coords, item);
+    });
+  });
+};
 
 export default compBoardView;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,29 @@
-import Gameboard from './Gameboard';
-import Game from './Game';
-import compBoardView from './compBoardView';
-import './styles.css';
-import PlayerBoardView from './playerBoardView';
-import placeShipsView from './placeShipsView';
+import Gameboard from "./Gameboard";
+import Game from "./Game";
+import compBoardView from "./compBoardView";
+import "./styles.css";
+import PlayerBoardView from "./playerBoardView";
+import placeShipsView from "./placeShipsView";
 
 function main() {
-    const game = Game();
-    document.addEventListener('DOMContentLoaded', () => {
-        PlayerBoardView(game.p1Board);
-        placeShipsView(game);
-        const reset = document.querySelector('#reset');
-        reset.onclick = () => {
-            const newGame = Game();
-            document.querySelector('.game-over').innerHTML ="";
-            const compboard = document.querySelector("#comp-board");
-            compboard.innerHTML ="";
-            PlayerBoardView(newGame.p1Board);
-            placeShipsView(newGame);
-
-        };
-
-            
-    });
-
+  const game = Game();
+  document.addEventListener("DOMContentLoaded", () => {
+    PlayerBoardView(game.p1Board);
+    placeShipsView(game);
+    const reset = document.querySelector("#reset");
+    reset.onclick = () => {
+      const newGame = Game();
+      const playerBoard = document.querySelector("#player-board");
+      playerBoard.classList.remove("pieces-placed");
+      document.querySelector(".game-over").innerHTML = "";
+      const compboard = document.querySelector("#comp-board");
+      compboard.innerHTML = "";
+      PlayerBoardView(newGame.p1Board);
+      placeShipsView(newGame);
+    };
+  });
 }
-
 
 main();
 
 export default main;
-
-
-
